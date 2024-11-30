26ee67fa4127603678481edfc4e70bc4d60f32ad
Ivisson Pereira <ivipnascimento@hotmail.com>
2024-11-30 04:45:35 -0300

    fix: fixing dependencies on vercel deploy, solving google-generative-ai and chunking

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,28 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
-      external: ['react-leaflet', 'papaparse', '@google/generative-ai'],
+      external: ['react-leaflet', 'papaparse'],
       output: {
         globals: {
           'react-leaflet': 'ReactLeaflet',
-          'papaparse': 'Papa',
-          '@google/generative-ai': 'GoogleGenerativeAI'
+          'papaparse': 'Papa'
+        },
+        manualChunks(id) {
+          if (id.includes('@google/generative-ai')) {
+            return 'google-ai'
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
         }
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@google/generative-ai': '@google/generative-ai/dist/index.js'
     }
   },
   optimizeDeps: {
